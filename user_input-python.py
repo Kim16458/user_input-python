a19821c18bb26e7b25adc01f8c8e384c62d8d417
Kim16458 <kirimiochola@gmail.com>
2024-02-23 22:18:44 +0300

    user-input

---
+++ user_input-python.py
@@ -0,0 +1,9 @@
+
+name = input("What is your name?")
+age = int(input("How old are you? "))
+location = input("Where do you live?")
+
+result = f"Good morning {name}. I am python and you are a {age} year old from {location}. Hope you have a nice day."
+
+print(result)
+
